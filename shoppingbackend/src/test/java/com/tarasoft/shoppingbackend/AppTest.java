c35d5145e4208d35d9a8cc2a595f01e6bb0ffe9b
Avnish <avniniet@gmail.com>
2018-08-02 22:47:30 +0530

    Project st up completed successfully

---
+++ shoppingbackend/src/test/java/com/tarasoft/shoppingbackend/AppTest.java
@@ -0,0 +1,20 @@
+package com.tarasoft.shoppingbackend;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
